e008de51c00e39641a31618881a9f4777c5dbb78
Mouhamadi Ousseni <mousse56@r16.script.univ-paris-diderot.fr>
2019-03-04 15:02:55 +0100

    Nouvelle version

---
+++ code.py
@@ -0,0 +1,51 @@
+"""
+print(2)
+
+""
+ma_variable = 5
+if ma_variable + 12 < 54:
+    print("plus petit")
+else:
+    print("plus grand")
+"""
+
+
+"""
+
+ma_variable = 5
+if ma_variable + 5 == 10:
+    print("egaux")
+else:
+    print("nul")
+
+
+
+""""""
+liste_invites = "MArcel","HIppolyte","Berthe",
+for invite in liste_invites:
+    print("Bonjour "+invite)
+
+
+for compteur in range(10):
+    print(compteur)
+"""
+
+
+
+def ma_fonction(param):
+    if param < 10:
+        print(param)
+    else :
+        print("rien")
+
+resultat = ma_fonction(param=6)
+
+"""
+def ma_function(param):
+    if param == 9:
+        print(9)
+    else :
+        print(param+1)
+
+resultat = ma_function(param=5)
+"""
